10369a23099fc1f135f40d8b5bb36100b3eae69d
Cooper Hanke <hanke31314@gmail.com>
2018-02-11 18:17:57 -0600

    UI improvement by removing status bar once app starts

--- app/src/main/java/com/example/cooper/scorecounter/MainActivity.java
+++ app/src/main/java/com/example/cooper/scorecounter/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.cooper.scorecounter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,6 +14,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
         displayHomeScore(homeScore);
         displayVisitorScore(visitorScore);
